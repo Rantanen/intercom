e49f0568f6accc40b38b983a244c108517957d73
Mikko Rantanen <jubjub@jubjubnest.net>
2017-11-06 21:51:37 +0200

    Try enabling C# tests in Appveyor

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -5,3 +5,9 @@ pushd test\cpp\x64\Release
 
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile( "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\output.xml))
+
+popd
+
+pushd test\cs\bin\x64\Release
+vstest.console /logger:Appveyor cs.dll
+popd
