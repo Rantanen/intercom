fabecfcdda9d8320fa8b32c6d11f7bd33b9b23bb
Juha Lepola <juha.lepola@gmail.com>
2018-07-19 20:46:49 +0300

    Export the lone function in "runpath" library
    MSVC does not generate the import library unless there is at least one exported function.

--- test/runpath/init.h
+++ test/runpath/init.h
@@ -1,6 +1,21 @@
 
+// Functions need to be explicitly exported on Windows in order to the generate import library file.
+#ifdef _MSC_VER
+    #define EXPORT_API __declspec( dllexport )
+#else
+    #define EXPORT_API
+#endif
+
 /**
  * @brief Expose a symbol which other binaries can callo to ensure libpath is linked.
  *
  */
-extern "C" void init_runpath();
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    EXPORT_API void init_runpath();
+
+#ifdef __cplusplus
+}
+#endif
