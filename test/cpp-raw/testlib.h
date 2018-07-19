009eed91e15e5cb9293d9ab0316f7077bd0fa25a
Juha Lepola <juha.lepola@gmail.com>
2018-07-19 21:37:41 +0300

    Fixed a warning when importing test_lib.

--- test/cpp-raw/testlib.h
+++ test/cpp-raw/testlib.h
@@ -4,8 +4,12 @@
 #define INTERCOM_FLATTEN_DECLARATIONS
 #ifdef _MSC_VER
     #include <Windows.h>
-    #import "..\testlib\target\debug\test_lib.dll" raw_interfaces_only named_guids
+
+    // Importing the DLL duplicates the __oud identifier which causes a warning.
+    #import "..\testlib\target\debug\test_lib.dll" raw_interfaces_only named_guids \
+        rename("__out", "__out_test_lib")
     using namespace TestLib;
+
 #elif __GNUC__
     #include "../cpp-utility/generated/test_lib.h"
 #else
