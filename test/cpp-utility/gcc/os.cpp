3019897af50e9880ad18580c7d31f81fdbc3aacb
Juha Lepola <juha.lepola@gmail.com>
2018-07-19 12:09:41 +0300

    Ensure references to "runpath" library are not optimized away
    This is a test to check if the linker optimizes away references to
    the libraries that are not called directly in Travis CI.

--- test/cpp-utility/gcc/os.cpp
+++ test/cpp-utility/gcc/os.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 
 #include "../os.h"
+#include "../../runpath/init.h"
 #include "test_lib.h"
 #include "../../../intercom-cpp/src/cominterop.h"
 #include "../../../intercom-cpp/src/activator.h"
@@ -12,6 +13,8 @@ using intercom::Activator;
 
 void InitializeRuntime()
 {
+    // Ensure the library "runpath" won't get optimized away.
+    init_runpath();
 }
 
 void UninitializeRuntime()
