f3a11f20a4c94c9013774c75b743e302c20c1395
Juha Lepola <juha.lepola@gmail.com>
2018-07-19 21:27:38 +0300

    Ensure the COM objects get destroyed before COM runtime is uninitialized
    Failing to do so causes segmentation fault.

--- test/cpp-wrapper/interface_wrappers.cpp
+++ test/cpp-wrapper/interface_wrappers.cpp
@@ -14,98 +14,102 @@ TEST_CASE( "Using interface wrappers works" )
     // Initialize COM.
     InitializeRuntime();
 
-    intercom::ClassFactory< test_lib::raw::RefCountOperationsDescriptor > refCountFactory;
-    intercom::RawInterface<IRefCountOperations> refCountOps;
-    intercom::HRESULT created = refCountFactory.create( IRefCountOperations::ID, refCountOps.out() );
-    REQUIRE( intercom::SC_OK == created );
-    REQUIRE( static_cast< bool >( refCountOps ) );
-    REQUIRE( refCountOps->GetRefCount() == 1 );
-
-    SECTION( "Acquiring another reference succeeds." )
+    // Ensure all the COM classes get destroyed before the runtime is uninitialized.
     {
-        intercom::RawInterface<IRefCountOperations> refCountOpsDuplicate = refCountOps;
-        REQUIRE( static_cast< bool >( refCountOpsDuplicate ) );
-        REQUIRE( refCountOps.get() == refCountOpsDuplicate.get() );
-
-        // There should be another reference now.
-        REQUIRE( refCountOpsDuplicate->GetRefCount() == 2 );
-    }
-
-    SECTION( "Moving a reference to another variable succeeds." )
-    {
-        intercom::RawInterface<IRefCountOperations> refCountOpsMoved = std::move( refCountOps );
-        REQUIRE( static_cast< bool >( refCountOpsMoved ) );
-        REQUIRE( static_cast< bool >( refCountOps ) == false );
-        REQUIRE( refCountOpsMoved->GetRefCount() == 1 );
-    }
-
-    SECTION( "Releasing a variable succeeds." )
-    {
-        IRefCountOperations* released = refCountOps.release();
-        REQUIRE( static_cast< bool >( refCountOps ) == false );
-        REQUIRE( released->GetRefCount() == 1 );
-        REQUIRE( released->Release() == 0 );
-    }
-
-    SECTION( "Reseting a variable to nullptr succeeds." )
-    {
-        refCountOps.reset();
-        REQUIRE( static_cast< bool >( refCountOps ) == false );
-    }
-
-    SECTION( "Reseting a variable to another variable succeeds." )
-    {
-        intercom::RawInterface<IRefCountOperations> anotherCounter =
-                refCountFactory.create< test_lib::raw::IRefCountOperations >();
-        REQUIRE( anotherCounter->GetRefCount() == 1 );
-        REQUIRE( refCountOps.get() != anotherCounter.get() );
-        refCountOps.reset( anotherCounter.get() );
+        intercom::ClassFactory< test_lib::raw::RefCountOperationsDescriptor > refCountFactory;
+        intercom::RawInterface<IRefCountOperations> refCountOps;
+        intercom::HRESULT created = refCountFactory.create( IRefCountOperations::ID, refCountOps.out() );
+        REQUIRE( intercom::SC_OK == created );
         REQUIRE( static_cast< bool >( refCountOps ) );
-        REQUIRE( anotherCounter.get() == refCountOps.get() );
-        REQUIRE( anotherCounter->GetRefCount() == 2 );
-    }
-
-    SECTION( "Calculating hash code succeeds." )
-    {
-        size_t expectedHash = std::hash< IRefCountOperations* >{}( refCountOps.get() );
-        REQUIRE( expectedHash == std::hash< intercom::RawInterface<IRefCountOperations>  >{}(
-                 refCountOps ) );
-    }
-
-    SECTION( "Swapping variables succeeds." )
-    {
-        intercom::RawInterface<IRefCountOperations> anotherCounter =
-                refCountFactory.create< test_lib::raw::IRefCountOperations >();
-
-        // Determine expected values after the swap operation.
-        IRefCountOperations* expectedAnother = refCountOps.get();
-        IRefCountOperations* expectedRefCount = anotherCounter.get();
-
-        // Verify the swap succeeds.
-        std::swap( refCountOps, anotherCounter );
-        REQUIRE( expectedAnother == anotherCounter.get() );
-        REQUIRE( expectedRefCount == refCountOps.get() );
-        REQUIRE( anotherCounter->GetRefCount() == 1 );
         REQUIRE( refCountOps->GetRefCount() == 1 );
-    }
 
-    SECTION( "Creating an object with non-existant interface throws correct exception." )
-    {
-        REQUIRE_THROWS_AS( refCountFactory.create< cppraw::utility::IDummyInterface >(), intercom::NoSuchInterface );
-    }
+        SECTION( "Acquiring another reference succeeds." )
+        {
+            intercom::RawInterface<IRefCountOperations> refCountOpsDuplicate = refCountOps;
+            REQUIRE( static_cast< bool >( refCountOpsDuplicate ) );
+            REQUIRE( refCountOps.get() == refCountOpsDuplicate.get() );
 
-    SECTION( "Requesting unimplemented interface in creation fails appropriately." )
-    {
-        try
-        {
-            intercom::RawInterface<ISharedInterface> wrongInterface =
-                    refCountFactory.create< test_lib::raw::ISharedInterface >();
-            FAIL( "Requesting invalid interface succeeded." );
+            // There should be another reference now.
+            REQUIRE( refCountOpsDuplicate->GetRefCount() == 2 );
         }
-        catch( intercom::NoSuchInterface& ex )
+
+        SECTION( "Moving a reference to another variable succeeds." )
         {
-            REQUIRE( ex.error_code() == intercom::EC_NOINTERFACE );
+            intercom::RawInterface<IRefCountOperations> refCountOpsMoved = std::move( refCountOps );
+            REQUIRE( static_cast< bool >( refCountOpsMoved ) );
+            REQUIRE( static_cast< bool >( refCountOps ) == false );
+            REQUIRE( refCountOpsMoved->GetRefCount() == 1 );
         }
+
+        SECTION( "Releasing a variable succeeds." )
+        {
+            IRefCountOperations* released = refCountOps.release();
+            REQUIRE( static_cast< bool >( refCountOps ) == false );
+            REQUIRE( released->GetRefCount() == 1 );
+            REQUIRE( released->Release() == 0 );
+        }
+
+        SECTION( "Reseting a variable to nullptr succeeds." )
+        {
+            refCountOps.reset();
+            REQUIRE( static_cast< bool >( refCountOps ) == false );
+        }
+
+        SECTION( "Reseting a variable to another variable succeeds." )
+        {
+            intercom::RawInterface<IRefCountOperations> anotherCounter =
+                    refCountFactory.create< test_lib::raw::IRefCountOperations >();
+            REQUIRE( anotherCounter->GetRefCount() == 1 );
+            REQUIRE( refCountOps.get() != anotherCounter.get() );
+            refCountOps.reset( anotherCounter.get() );
+            REQUIRE( static_cast< bool >( refCountOps ) );
+            REQUIRE( anotherCounter.get() == refCountOps.get() );
+            REQUIRE( anotherCounter->GetRefCount() == 2 );
+        }
+
+        SECTION( "Calculating hash code succeeds." )
+        {
+            size_t expectedHash = std::hash< IRefCountOperations* >{}( refCountOps.get() );
+            REQUIRE( expectedHash == std::hash< intercom::RawInterface<IRefCountOperations>  >{}(
+                    refCountOps ) );
+        }
+
+        SECTION( "Swapping variables succeeds." )
+        {
+            intercom::RawInterface<IRefCountOperations> anotherCounter =
+                    refCountFactory.create< test_lib::raw::IRefCountOperations >();
+
+            // Determine expected values after the swap operation.
+            IRefCountOperations* expectedAnother = refCountOps.get();
+            IRefCountOperations* expectedRefCount = anotherCounter.get();
+
+            // Verify the swap succeeds.
+            std::swap( refCountOps, anotherCounter );
+            REQUIRE( expectedAnother == anotherCounter.get() );
+            REQUIRE( expectedRefCount == refCountOps.get() );
+            REQUIRE( anotherCounter->GetRefCount() == 1 );
+            REQUIRE( refCountOps->GetRefCount() == 1 );
+        }
+
+        SECTION( "Creating an object with non-existant interface throws correct exception." )
+        {
+            REQUIRE_THROWS_AS( refCountFactory.create< cppraw::utility::IDummyInterface >(), intercom::NoSuchInterface );
+        }
+
+        SECTION( "Requesting unimplemented interface in creation fails appropriately." )
+        {
+            try
+            {
+                intercom::RawInterface<ISharedInterface> wrongInterface =
+                        refCountFactory.create< test_lib::raw::ISharedInterface >();
+                FAIL( "Requesting invalid interface succeeded." );
+            }
+            catch( intercom::NoSuchInterface& ex )
+            {
+                REQUIRE( ex.error_code() == intercom::EC_NOINTERFACE );
+            }
+        }
+
     }
 
     UninitializeRuntime();
