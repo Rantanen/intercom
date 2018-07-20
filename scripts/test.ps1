3a8fa3a78232257afe9979901ec1e70d89684988
Juha Lepola <juha.lepola@gmail.com>
2018-07-20 10:56:49 +0300

    Run all unit tests in AppVeyor
    This also changes the target location of the binaries
    out of the build area.

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,12 +1,25 @@
-pushd test\cpp-raw\msvc\x64\Release
-./cpp.exe
-$cpp_ok = $?
+pushd bin\AMD64\Release
+./cpp-raw.exe
+$cpp_raw_ok = $?
+./cpp-wrapper.exe
+$cpp_wrapper_ok = $?
+./cpp-dl.exe
+$cpp_dl_ok = $?
+
 
 # Upload test results only if we're running on AppVeyor.
 if ( Test-Path env:APPVEYOR_JOB_ID ) {
-    ./cpp.exe -r junit > output.xml
+    ./cpp-raw.exe -r junit > output_raw.xml
     $wc = New-Object 'System.Net.WebClient'
-    $wc.UploadFile( "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\output.xml))
+    $wc.UploadFile( "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\output_raw.xml))
+
+    ./cpp-wrapper.exe -r junit > output_wrapper.xml
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile( "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\output_wrapper.xml))
+
+    ./cpp-dl.exe -r junit > output_dl.xml
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile( "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\output_dl.xml))
 }
 
 popd
@@ -23,6 +36,6 @@ if ( Test-Path env:APPVEYOR_JOB_ID ) {
 
 popd
 
-if( -not ( $cpp_ok -and $cs_ok ) ) {
+if( -not ( $cpp_raw_ok -and $cpp_wrapper_ok -and $cpp_dl_ok -and $cs_ok ) ) {
     exit -1
 }
