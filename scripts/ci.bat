28bb2efff0a94709e1c6256b3b6d69ffb6db6cd2
Mikko Rantanen <jubjub@jubjubnest.net>
2018-10-14 06:24:41 +0300

    Remove needless compilations

--- scripts/ci.bat
+++ scripts/ci.bat
@@ -10,34 +10,6 @@ REM Build Intercom
 cargo build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM Build testlib
-pushd test\testlib
-
-cargo build
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-popd
-
-REM Build multilib
-pushd test\multilib
-
-cargo build
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-popd
-
-
-REM Generate IDL and Manifest for the testlib.
-pushd intercom-cli
-
-cargo run -- idl ..\test\testlib > ..\test\testlib\testlib.idl
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cargo run -- manifest ..\test\testlib > ..\test\testlib\TestLib.Assembly.manifest
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-popd
-
 REM Build C++ test suite
 del /s /q build\x64
 mkdir build
