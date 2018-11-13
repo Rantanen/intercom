a348a1827a24cf0b28db9e1b1123046287bbfd11
Mikko Rantanen <jubjub@jubjubnest.net>
2018-11-13 03:21:24 +0200

    Add IErrorInfo definition to posix

--- intercom-cpp/src/posix/isupporterrorinfo.hpp
+++ intercom-cpp/src/posix/isupporterrorinfo.hpp
@@ -1,15 +1,41 @@
 
-#ifndef INTERCOM_CPP_POSIX_IUNKNOWN_H
-#define INTERCOM_CPP_POSIX_IUNKNOWN_H
+#ifndef INTERCOM_CPP_POSIX_ISUPPORTERRORINFO_H
+#define INTERCOM_CPP_POSIX_ISUPPORTERRORINFO_H
 
 #include "../callingconvention.hpp"
 #include "../datatypes.hpp"
 #include "../error_codes.hpp"
 #include "../guiddef.hpp"
 
+#include "datatypes.hpp"
 
-// MIDL_INTERFACE("00000000-0000-0000-C000-000000000046")
-struct ISupportErrorInfo
+// MIDL_INTERFACE("1CF2B120-547D-101B-8E65-08002B2BD119")
+struct IErrorInfo : public IUnknown
+{
+public:
+
+	virtual intercom::HRESULT INTERCOM_CC GetGUID( 
+		intercom::GUID *pGUID) = 0;
+	
+	virtual intercom::HRESULT INTERCOM_CC GetSource( 
+		intercom::BSTR *pBstrSource) = 0;
+	
+	virtual intercom::HRESULT INTERCOM_CC GetDescription( 
+		intercom::BSTR *pBstrDescription) = 0;
+	
+	virtual intercom::HRESULT INTERCOM_CC GetHelpFile( 
+		intercom::BSTR *pBstrHelpFile) = 0;
+	
+	virtual intercom::HRESULT INTERCOM_CC GetHelpContext( 
+		intercom::DWORD *pdwHelpContext) = 0;
+};
+
+static const intercom::IID IID_IErrorInfo = {
+		0x1CF2B120, 0x547D, 0x101B,
+		{ 0x8E, 0x65, 0x08, 0x00, 0x2B, 0x2B, 0xD1, 0x19 } };
+
+// MIDL_INTERFACE("DF0B3D60-548F-101B-8E65-08002B2BD119" )
+struct ISupportErrorInfo : public IUnknown
 {
 public:
 
